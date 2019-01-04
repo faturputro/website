e725c66c71301f863bd4cd4898056822745e5310
Faturachman Dwi Putro <44432754+faturputro@users.noreply.github.com>
2019-01-04 17:22:17 +0700

    removed comments

--- js/main.js
+++ js/main.js
@@ -8,36 +8,30 @@
     "use strict";
 
     var cfg = {
-        scrollDuration : 800, // smoothscroll duration
+        scrollDuration : 800, 
         mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
     },
 
     $WIN = $(window);
 
-    // Add the User Agent to the <html>
-    // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
     var doc = document.documentElement;
     doc.setAttribute('data-useragent', navigator.userAgent);
 
 
-   /* Preloader
-    * -------------------------------------------------- */
+  
     var clPreloader = function() {
 
         $("html").addClass('cl-preload');
 
         $WIN.on('load', function() {
 
-            //force page scroll position to top at page refresh
-            // $('html, body').animate({ scrollTop: 0 }, 'normal');
-
-            // will first fade out the loading animation
+            
             $("#loader").fadeOut("slow", function() {
-                // will fade out the whole DIV that covers the website.
+               
                 $("#preloader").delay(300).fadeOut("slow");
             });
 
-            // for hero content animations
+           
             $("html").removeClass('cl-preload');
             $("html").addClass('cl-loaded');
 
@@ -45,8 +39,6 @@
     };
 
 
-   /* Menu on Scrolldown
-    * ------------------------------------------------------ */
     var clMenuOnScrolldown = function() {
 
         var menuTrigger = $('.header-menu-toggle');
@@ -64,8 +56,6 @@
     };
 
 
-   /* OffCanvas Menu
-    * ------------------------------------------------------ */
     var clOffCanvas = function() {
 
         var menuTrigger     = $('.header-menu-toggle'),
@@ -339,9 +329,9 @@ $(window).on('scroll', function(){
     var clBackToTop = function() {
 
         var pxShow  = 500,         // height on which the button will show
-        fadeInTime  = 400,         // how slow/fast you want the button to show
-        fadeOutTime = 400,         // how slow/fast you want the button to hide
-        scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+        fadeInTime  = 400,         // how slow/fast the button to show
+        fadeOutTime = 400,         // how slow/fast the button to hide
+        scrollSpeed = 300,         // how slow/fast the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
         goTopButton = $(".go-top")
 
         // Show or hide the sticky footer button
